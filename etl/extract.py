fe496f3ab4199400aba19cc577eaf8113abc7180
Michael Hammond <michaelhammond@macbook-jolllof.myfiosgateway.com>
2025-06-27 11:45:13 -0400

    added code to pull my user info and tested out getting following

--- etl/extract.py
+++ etl/extract.py
@@ -1,16 +1,53 @@
 import argparse
 import os
 from datetime import datetime, timedelta
+from requests_oauthlib import OAuth1
 
 from utilities import *
+os.system('clear')
 
 import structlog
 logger = structlog.get_logger()
 
+consumer_key='a2A6kLFlbDNJMiuI3Sl1BDxLE'
+consumer_secret='bWP0qEmf7xSygdVrMXVKHadrHcTpCRgTVabn9RUzXwCfxPObfK'
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAMat2gEAAAAAbSXpy%2FQ8lFwAwlYH6%2BDXgA9R5dc%3DvhRoAfdvyUhhbETcVPaDhvMk3NsqrP9EBjHukbjZYUYbHfwThY'
+access_token='1936517666514104320-vBYJwuRZFLDKumokPNARX3wPv5te3s'
+access_token_secret='3I6YzB5eQPocJPX48BloZBtB0ia8yseOlkKTSMti1xkG3'
+
 def main(args):
     logger.info("Initiating Data Extract")
+    base_url = 'https://api.twitter.com/2'
+    users = '/users/me'
+    following = '/users/:id/following'
     
+    auth = OAuth1(consumer_key, consumer_secret, access_token, access_token_secret)
 
+    
+    # Get user data
+    logger.info("Fetching user data")
+    url = f"{base_url}{users}"
+    logger.info(f"URL: {url}")
+    res = get_data(url, auth)
+    user_id = res.json().get('data', {}).get('id', None)
+    name= res.json().get('data', {}).get('name', None)
+    username= res.json().get('data', {}).get('username', None)
+
+    # Get following data
+
+    # logger.info("Fetching following data")
+    # url = f"{base_url}{following.replace(':id', user_id)}"
+    # params = {
+    #     "max_results": 1000,
+    #     "user.fields": "id,name,username,verified",
+    # }
+    # logger.info(f"URL: {url}")
+
+    # res = get_data(url, auth)
+    # logger.info(f"Response: {res}")
+
+    
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -29,3 +66,5 @@ if __name__ == "__main__":
         args = {v.split("=", 1)[0]: v.split("=", 1)[1] for v in list(args.set)}
 
     main(args)
+
+
--- utilities.py
+++ utilities.py
@@ -2,8 +2,21 @@ import requests
 import structlog
 logger = structlog.get_logger()
 
-def get_data(url):
-    response = requests.get(url)
+
+def get_data(url, auth):
+    response = requests.get(url, auth=auth)
+    logger.info(response)
+
+    if response.status_code != 200:
+        print(f"Error Fetching {url} \n {response}")
+        return []
+    else:
+        return response
+        logger.info(f"Utilities: Successfully fetched data from {url}")
+
+
+def post_data(url, auth, params):
+    response = requests.post(url, auth=auth, params=params)
 
     if response.status_code != 200:
         print(f"Error Fetching {url} \n {response}")
